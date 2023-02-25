19ae3d64cccb386af1df15c5df5b8c5f84d28288
mishowl <micacortezano1@gmail.com>
2023-02-25 17:38:29 +0800

    backend oksss

--- models/empLog.model.php
+++ models/empLog.model.php
@@ -8,31 +8,37 @@ class empLogModel{
         
         $stmt = (new Connection)->connect()->prepare("SELECT * FROM empwork WHERE empID = $empID");
         $stmt->execute();
-        $row = $stmt->fetch();
-        
-        // morning (8:31 - 8:45 AM) - considered late, otherwise it's a half day morning absent
-        // afternoon (1:31 - 1:45 PM) - considered late, , otherwise it's a half day afternoon absent
-        // (log-in without logout) or (log-out without log-in) - considered absent either morning or afternoon
+        $rows = $stmt->fetchAll();
 
-        //sample data only
-        $timeInMorning = strtotime("08:31:00");
-        $timeOutMorning = strtotime("12:31:00");
-        $timeInAfternoon = strtotime("13:31:00");
-        $timeOutAfternoon = strtotime("15:31:00");
+        foreach ($rows as $row) {
+            $time = strtotime($row['date'] . ' ' . $row['time']);
 
-        //declarations
-        $morningLate = strtotime("08:30:00");
-        $morningAbsent = strtotime("08:45:00");
-        $afternoonLate = strtotime("13:30:00");
-        $afternoonAbsent = strtotime("13:45:00");
+            if(date("Y-m-d H", $time) == $row['date'].' 08'){
+                $timeInMorning = $time;
+            } elseif(date("Y-m-d H", $time) == $row['date'].' 12'){
+                $timeOutMorning = $time;
+            } elseif(date("Y-m-d H", $time) == $row['date'].' 13'){
+                $timeInAfternoon = $time;
+            } elseif(date("Y-m-d H", $time) == $row['date'].' 17'){
+                $timeOutAfternoon = $time;
+            }
+
+            //declarations
+            $refDate = $row['date'];
+            $morningLate = strtotime($refDate." 08:31:00");
+            $morningAbsent = strtotime($refDate." 08:46:00");
+            $afternoonLate = strtotime($refDate." 13:31:00");
+            $afternoonAbsent = strtotime($refDate." 13:46:00");
+        }
 
         //morning
         if (isset($timeInMorning) and isset($timeOutMorning)){
             if ($timeInMorning <=  $morningLate){
                 $statusMorning = "on-time";
+                $minsLateMorning = 0;
             } elseif (($timeInMorning >= $morningLate) and ($timeInMorning <= $morningAbsent)){
                 $statusMorning = "late";
-                $minsLateMorning = floor(abs($morningLate - $timeInMorning)/60);
+                $minsLateMorning = round(abs($morningLate - $timeInMorning)/60);
             } else {
                 $statusMorning = "absent";
             }
@@ -43,10 +49,11 @@ class empLogModel{
         //afternoon
         if (isset($timeInAfternoon) and isset($timeOutAfternoon)){
             if ($timeInAfternoon <= $afternoonLate){
-                $statusAfternoon = "on-time";
+                $statusAfternoon= "on-time";
+                $minsLateAfternoon = 0;
             } elseif (($timeInAfternoon >= $afternoonLate) and ($timeInAfternoon <= $afternoonAbsent)){
                 $statusAfternoon = "late";
-                $minsLateAfternoon = floor(abs($afternoonLate - $timeInAfternoon)/60);
+                $minsLateAfternoon = round(abs($afternoonLate - $timeInAfternoon)/60);
             } else {
                 $statusAfternoon = "absent";
             }
@@ -56,19 +63,28 @@ class empLogModel{
 
         //calculate hours worked
         if ($statusMorning != "absent"){
-            $totalHoursMorning = floor(abs($timeInMorning - $timeOutMorning)/3600);
+            $totalHoursMorning = round(abs($timeInMorning - $timeOutMorning)/3600);
         } else {
+            $minsLateMorning = 0;
             $totalHoursMorning = 0;
         }
         if ($statusAfternoon != "absent"){
-            $totalHoursAfternoon = floor(abs($timeInAfternoon - $timeOutAfternoon)/3600);
+            $totalHoursAfternoon = round(abs($timeInAfternoon - $timeOutAfternoon)/3600);
         } else {
+            $minsLateAfternoon = 0;
             $totalHoursAfternoon = 0;
         }
-        $totalHours = $totalHoursMorning + $totalHoursAfternoon . " hours";
-        $totalMinsLate = $minsLateMorning + $minsLateAfternoon . " minutes";
 
-        $empLog = array($totalHours, $totalMinsLate);
+        $logDate = $refDate;
+        if(isset($timeInMorning)){$timeInMorning = date("h:i:s a", $timeInMorning);}else{$timeInMorning="N/A";}
+        if(isset($timeOutMorning)){$timeOutMorning = date("h:i:s a", $timeOutMorning);}else{$timeOutMorning="N/A";}
+        if(isset($timeInAfternoon)){$timeInAfternoon = date("h:i:s a", $timeInAfternoon);}else{$timeInAfternoon="N/A";}
+        if(isset($timeOutAfternoon)){$timeOutAfternoon = date("h:i:s a", $timeOutAfternoon);}else{$timeOutAfternoon="N/A";}
+
+        $totalHours = $totalHoursMorning + $totalHoursAfternoon . " hour/s";
+        $totalMinsLate = $minsLateMorning + $minsLateAfternoon . " minute/s";
+
+        $empLog = array($totalHours, $totalMinsLate, $logDate, $timeInMorning, $timeOutMorning, $statusMorning, $timeInAfternoon, $timeOutAfternoon, $statusAfternoon);
         echo json_encode($empLog);
     }
 }
--- views/js/script.js
+++ views/js/script.js
@@ -9,15 +9,33 @@ $(function() {
             url: "ajax/getEmpLog_ajax.php",
             data: {"empID" : empID},
             cache: false,
-            // contentType: false,
-            // processData: false,
             dataType:"json",
             success: function(data){
                 document.getElementById('hoursWorked').value = data[0];
                 document.getElementById('minsLate').value = data[1];
+                document.getElementById('logDate').value = data[2];
+                document.getElementById('morningTimeIn').value = data[3];
+                document.getElementById('morningTimeOut').value = data[4];
+                document.getElementById('morningStatus').value = data[5];
+                document.getElementById('afternoonTimeIn').value = data[6];
+                document.getElementById('afternoonTimeOut').value = data[7];
+                document.getElementById('afternoonStatus').value = data[8];
             },
             error: function () {
-                alert ("Error");
+                document.getElementById('hoursWorked').value = "";
+                document.getElementById('minsLate').value ="";
+                document.getElementById('logDate').value = "";
+                document.getElementById('morningTimeIn').value = "";
+                document.getElementById('morningTimeOut').value = "";
+                document.getElementById('morningStatus').value = "";
+                document.getElementById('afternoonTimeIn').value = "";
+                document.getElementById('afternoonTimeOut').value = "";
+                document.getElementById('afternoonStatus').value = "";
+
+                swal.fire({
+                    icon: 'error',
+                    text: 'Employee not found in Database!'
+                })
             }, complete: function(){}
         });
     });
